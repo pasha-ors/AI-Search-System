130bff5343e6e4afed335d3324b3e062ab65230a
Pavlo <pavlo.orsahosh05@gmail.com>
2026-05-05 12:44:39 +0200

    initial project structure

---
+++ product-service/src/test/java/com/ai_search/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_search.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
